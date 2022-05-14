1d779fad1dcb810052fdcaf22c3ce8f14633a9a9
rezainiet <masudrezaog@gmail.com>
2022-05-15 01:06:58 +0600

    new file updated

--- __server_deploy.js
+++ __server_deploy.js
@@ -1,3 +1,15 @@
 /**
- * 
+ * --------------------------
+ * ONE TIME FOR MY COMPUTER
+ * --------------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * --------------------------
+ * For each project one time
+ * --------------------------
+ * 1. heroku create
+ * 2. make sure: git add. git commit. git push
+ * 3. git push heroku main
  */
